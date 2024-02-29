e98b59e223d48390da5d6f88a1a2950c27796f1f
drisspg <drisspguessous@gmail.com>
2024-02-29 15:32:02 -0800

    remove add fluff

--- driss_torch/__init__.py
+++ driss_torch/__init__.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Optional
 
 import torch
 
@@ -17,17 +16,6 @@ def list_ops():
     return ops.__dir__()
 
 
-def add_one(x: torch.Tensor) -> torch.Tensor:
-    """Add one to a tensor.
-    This is a dummy test op to demonstrate how to add custom ops to PyTorch.
-    Args:
-        x: The input tensor.
-    Returns:
-        The output tensor.
-    """
-    return ops.add_one(x)
-
-
 def saturated_cast(
     x: torch.Tensor,
     scale: torch.Tensor,
@@ -38,7 +26,7 @@ def saturated_cast(
     Args;
         x: The input tensor.
         out_dtype: The output data type, must be a float8 dtype.
-        scale: An optional on device tensor, this is expected to be a singleton tensor whose value will be multiplied
+        scale: An on device tensor, this is expected to be a singleton tensor whose value will be multiplied
             x before casting
         transpose: If true will transpose the input tensor during casting
     Returns:
