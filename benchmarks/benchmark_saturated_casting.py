1dc439defa057d0ba4a4b646abc5da2ad9aa6c8c
drisspg <drisspguessous@gmail.com>
2024-02-29 21:23:50 -0800

    ❯ python /home/drisspg/meta/driss_torch/benchmarks/benchmark_saturated_casting.py 100%|████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 22/22 [01:14<00:00,  3.40s/it]   num_rows    num_cols  high_precision_dtype    low_precision_dtype      cuda_time    pytorch_time    compiled_pytorch_time ----------  ----------  ----------------------  ---------------------  -----------  --------------  -----------------------        512         512  torch.bfloat16          torch.float8_e4m3fn        5.46512         72.2336                  59.9502        512         512  torch.bfloat16          torch.float8_e5m2          5.50805         72.5867                  59.5001       1024        1024  torch.bfloat16          torch.float8_e4m3fn        5.63875         72.5901                  64.8824       1024        1024  torch.bfloat16          torch.float8_e5m2          5.61068         73.4559                  64.987       2048        2048  torch.bfloat16          torch.float8_e4m3fn        6.92928         73.2639                  21.8212       2048        2048  torch.bfloat16          torch.float8_e5m2          6.92831         73.238                   21.7541       1024        8192  torch.bfloat16          torch.float8_e4m3fn       11.6602         125.987                   21.8744       1024        8192  torch.bfloat16          torch.float8_e5m2         11.662          126.608                   21.9473       8192        1280  torch.bfloat16          torch.float8_e4m3fn       13.9878         160.16                    21.9679       8192        1280  torch.bfloat16          torch.float8_e5m2         13.9841         160.584                   22.0702       8192        7168  torch.bfloat16          torch.float8_e4m3fn       81.9416         936.225                   85.1023       8192        7168  torch.bfloat16          torch.float8_e5m2         82.5054         938.797                   85.1112       3584        8192  torch.bfloat16          torch.float8_e4m3fn       43.5017         486.14                    44.7887       3584        8192  torch.bfloat16          torch.float8_e5m2         43.4985         486.918                   44.8074       2048      109760  torch.bfloat16          torch.float8_e4m3fn      301.522         3493.18                   317.802       2048      109760  torch.bfloat16          torch.float8_e5m2        300.25          3495.29                   319.173          1        3232  torch.bfloat16          torch.float8_e4m3fn        5.54213         71.9274                  64.7877          1        3232  torch.bfloat16          torch.float8_e5m2          5.62434         72.1416                  64.597       2048           1  torch.bfloat16          torch.float8_e4m3fn        5.59367         71.5838                  63.776       2048           1  torch.bfloat16          torch.float8_e5m2          5.59475         71.9973                  63.966      14144        2048  torch.bfloat16          torch.float8_e4m3fn       42.9653         479.963                   44.3175      14144        2048  torch.bfloat16          torch.float8_e5m2         42.9723         480.981                   44.2864

--- benchmarks/benchmark_saturated_casting.py
+++ benchmarks/benchmark_saturated_casting.py
@@ -6,6 +6,7 @@ from typing import List
 import torch
 
 from driss_torch import saturated_cast
+from float8_experimental.float8_utils import amax_to_scale
 from jsonargparse import CLI
 
 from tabulate import tabulate
@@ -22,16 +23,17 @@ torch._dynamo.config.cache_size_limit = 1000
 
 def eager_scaled_quant(
     a: torch.Tensor,
-    scale: torch.Tensor,
+    amax: torch.Tensor,
     fp8_dtype: torch.dtype,
 ):
     """Quantize tensor to fp8 using a delayed scaled and calculate abs_max
 
     Args:
         a: Input tensor to quantize
-        scale: Scale to apply to input tensor, calculated from previous abs_max
+        amax of the input tensor
         fp8_dtype: FP8 datatype to quantize to
     """
+    scale = amax_to_scale(amax, fp8_dtype, a.dtype)
     out = a * scale
     out = torch.where(out > torch.finfo(fp8_dtype).max, torch.finfo(fp8_dtype).max, out)
     out = torch.where(out < -1 * torch.finfo(fp8_dtype).max, -1 * torch.finfo(fp8_dtype).max, out)
@@ -97,21 +99,17 @@ def run_experiment(config: ExperimentConfig) -> ExperimentResult:
         config.num_rows, config.num_cols, dtype=config.high_precision_dtype, device=device
     )
     cuda_hp_tensor = high_precision_tensor.clone()
-    cuda_scale = torch.ones(1, dtype=torch.bfloat16, device=device)
+    cuda_amax = torch.abs(high_precision_tensor).max().to(torch.float32)
 
-    eager_abs_max = torch.abs(high_precision_tensor).max().to(torch.float32)
-
-    scale = torch.finfo(config.low_precision_dtype).max / eager_abs_max
-    scale = scale.to(torch.float32)
-    scale = torch.ones(1, dtype=torch.float32, device=device)
+    eager_amax = torch.abs(high_precision_tensor).max().to(torch.float32)
 
     # Correctness check:
-    cuda_out = saturated_cast(cuda_hp_tensor, config.low_precision_dtype, cuda_scale)
+    cuda_out = saturated_cast(cuda_hp_tensor, eager_amax, config.low_precision_dtype)
     cuda_out_hp = cuda_out.to(config.high_precision_dtype)
 
-    eager_out = eager_scaled_quant(high_precision_tensor, scale, config.low_precision_dtype).to(
-        config.high_precision_dtype
-    )
+    eager_out = eager_scaled_quant(
+        high_precision_tensor, eager_amax, config.low_precision_dtype
+    ).to(config.high_precision_dtype)
     eager_out_hp = eager_out.to(config.high_precision_dtype)
 
     torch.testing.assert_close(cuda_out_hp, eager_out_hp, rtol=1e-3, atol=1e-3)
@@ -119,20 +117,20 @@ def run_experiment(config: ExperimentConfig) -> ExperimentResult:
     cuda_time = benchmark_torch_function_in_microseconds(
         saturated_cast,
         cuda_hp_tensor,
+        eager_amax,
         config.low_precision_dtype,
-        cuda_scale,
     )
     pytorch_time = benchmark_torch_function_in_microseconds(
         eager_scaled_quant,
         high_precision_tensor,
-        scale,
+        eager_amax,
         config.low_precision_dtype,
     )
     compiled_pytorch_fn = torch.compile(eager_scaled_quant, fullgraph=True)
     compiled_pytorch_time = benchmark_torch_function_in_microseconds(
         compiled_pytorch_fn,
         high_precision_tensor,
-        scale,
+        eager_amax,
         config.low_precision_dtype,
     )
     return ExperimentResult(
