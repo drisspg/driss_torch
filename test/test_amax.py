e0e1bd0ea9a0aab043b1ac3dcfd176d6ec98afe0
drisspg <drisspguessous@gmail.com>
2024-06-16 14:21:27 -0700

    nvm eager was broke baby

--- test/test_amax.py
+++ test/test_amax.py
@@ -3,13 +3,17 @@ import torch
 from driss_torch import amax
 
 
-@pytest.mark.parametrize("num_rows", [3, 64, 512, 4096])
+def _eager_amax(a):
+    return torch.abs(a).max().to(torch.float32).expand(1)
+
+
+@pytest.mark.parametrize("num_rows", [4, 64, 512, 4096])
 @pytest.mark.parametrize("num_cols", [7, 17, 127, 512, 3212, 4097])
 @pytest.mark.parametrize("in_dtype", [torch.bfloat16, torch.float32])
 def test_cast(num_rows: int, num_cols: int, in_dtype: torch.dtype):
     a = torch.randn(num_rows, num_cols, dtype=in_dtype, device="cuda")
     custom_amax = amax(a)
-    eager_amax = torch.amax(a).to(torch.float32).expand(1)
+    eager_amax = _eager_amax(a)
     print(f"{custom_amax=}")
     print(f"{eager_amax=}")
     torch.testing.assert_close(custom_amax, eager_amax)
