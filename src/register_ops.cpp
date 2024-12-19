359cd53e2a7e49ab8adecded80b9295682a24601
drisspg <drisspguessous@gmail.com>
2024-12-19 09:41:35 -0800

    Show casing how to add fixed stride order
    stack-info: PR: https://github.com/drisspg/driss_torch/pull/10, branch: drisspg/stack/2

--- src/register_ops.cpp
+++ src/register_ops.cpp
@@ -8,7 +8,7 @@
 TORCH_LIBRARY(DrissTorch, m) {
   m.impl_abstract_pystub("driss_torch.abstract_impls");
   //   Saturated cast func from bf16 to fp8 types
-  m.def("saturated_cast(Tensor input, Tensor scale, ScalarType dtype, bool transpose) -> Tensor");
+  m.def("saturated_cast(Tensor input, Tensor scale, ScalarType dtype, bool transpose) -> Tensor", {at::Tag::needs_fixed_stride_order});
   m.impl("saturated_cast", c10::DispatchKey::CUDA, TORCH_FN(driss_torch::saturated_cast));
   //   Amax func
   m.def("amax(Tensor input) -> Tensor");
